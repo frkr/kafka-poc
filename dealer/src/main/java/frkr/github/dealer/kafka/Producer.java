c143de2b1e3a82722ab1d743b7af3dabc59c022f
DaviSM <davimesquita@gmail.com>
2019-09-21 23:01:28 -0300

    requestreply funcionando

--- dealer/src/main/java/frkr/github/dealer/kafka/Producer.java
+++ dealer/src/main/java/frkr/github/dealer/kafka/Producer.java
@@ -1,46 +1,49 @@
 package frkr.github.dealer.kafka;
 
 import frkr.github.ClienteRequest;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.integration.annotation.BridgeFrom;
-import org.springframework.integration.annotation.BridgeTo;
-import org.springframework.integration.channel.QueueChannel;
-import org.springframework.integration.core.MessagingTemplate;
-import org.springframework.integration.support.MessageBuilder;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHeaders;
-import org.springframework.messaging.PollableChannel;
-import org.springframework.stereotype.Service;
-import org.springframework.util.MimeTypeUtils;
+import org.springframework.kafka.requestreply.RequestReplyFuture;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
 
-import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
-@Service
+@Component
 public class Producer {
 
     @Autowired
-    private Topic topic;
+    private ReplyingKafkaTemplate<String, ClienteRequest, ClienteRequest> kafkaTemplate;
 
-    @Autowired
-    private ProducerFactory pf;
+    @Value("${kafka.topic.request-topic}")
+    private String requestTopic;
 
+    @Value("${kafka.topic.requestreply-topic}")
+    private String requestReplyTopic;
 
+    public ClienteRequest send(ClienteRequest clienteRequest) throws Exception {
+        // create producer record
+        ProducerRecord<String, ClienteRequest> record = new ProducerRecord<String, ClienteRequest>(requestTopic, clienteRequest);
+        // set reply topic in header
+        record.headers().add(new RecordHeader(KafkaHeaders.REPLY_TOPIC, requestReplyTopic.getBytes()));
+        // post in kafka topic
+        RequestReplyFuture<String, ClienteRequest, ClienteRequest> sendAndReceive = kafkaTemplate.sendAndReceive(record);
 
-    public void send(ClienteRequest clienteRequest) {
-        ReplyingKafkaTemplate template = new ReplyingKafkaTemplate(pf,);
-        topic.input().send(
-                MessageBuilder
-                        .withPayload(clienteRequest)
-                        .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
-                        .build()
-        );
-        Message<?> teste = pool().receive();
-        System.out.println(
-                teste.getPayload()
-        );
+        // confirm if producer produced successfully
+        //SendResult<String, ClienteRequest> sendResult = sendAndReceive.getSendFuture().get();
+
+        //print all headers
+        //sendResult.getProducerRecord().headers().forEach(header -> System.out.println(header.key() + ":" + header.value().toString()));
+
+        // get consumer record
+        ConsumerRecord<String, ClienteRequest> consumerRecord = sendAndReceive.get(30, TimeUnit.SECONDS);
+        // return consumer value
+        return consumerRecord.value();
     }
 
 }
--- dealer/src/main/java/frkr/github/dealer/rest/DealerService.java
+++ dealer/src/main/java/frkr/github/dealer/rest/DealerService.java
@@ -5,6 +5,7 @@ import frkr.github.DealerResponse;
 import frkr.github.dealer.kafka.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,8 +17,10 @@ public class DealerService {
     private Producer producer;
 
     @RequestMapping(value = "deal", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public DealerResponse deal(@RequestBody ClienteRequest clienteRequest) {
-        producer.send(clienteRequest);
+    public DealerResponse deal(@RequestBody ClienteRequest clienteRequest) throws Exception {
+        System.out.println(
+                producer.send(clienteRequest)
+        );
         return DealerResponse.builder()
                 .id(300L)
                 .build();
